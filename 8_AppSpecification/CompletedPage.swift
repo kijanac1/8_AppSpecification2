11fbbd09d0607f150b9f7decab346755ec426178
kijanac1 <kijanac1@yahoo.com>
2024-10-23 21:32:51 -0500

    Created completed page

---
+++ 8_AppSpecification/CompletedPage.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedPage.swift
+//  8_AppSpecification
+//
+//  Created by user264779 on 10/23/24.
+//
+
+import SwiftUI
+
+struct CompletedPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedPage()
+}
