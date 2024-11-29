4d904a276d9a3ccf9396d6d062118bd29eeb65b4
kijanac1 <kijanac1@yahoo.com>
2024-11-29 00:36:18 -0600

    Updated search position

---
+++ 8_AppSpecification/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  8_AppSpecification
+//
+//  Created by user267360 on 11/28/24.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
